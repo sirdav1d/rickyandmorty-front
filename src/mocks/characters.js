d0c94be3ce668a3fc8a65a0f1131461eb0da17e8
sirdav1d <ddavid.diniz@gmail.com>
2022-08-04 23:48:51 -0300

    chore: more cards; fix: size components

--- src/mocks/characters.js
+++ src/mocks/characters.js
@@ -2,7 +2,7 @@ export const charactersRam = [
   {
     nome: 'Morty Smith',
     descricao:
-      'Morty é emocional, ingênuo, obediente e facilmente manipulado, especialmente por Rick.',
+      'Morty é emocional, ingênuo, obediente e facilmente manipulado, especialmente por Rick',
     foto: require('../assets/img-ram/morty400x400.jpg'),
   },
   {
@@ -14,13 +14,25 @@ export const charactersRam = [
   {
     nome: 'Summer Smith',
     descricao:
-      'Summer é conhecida por sua personalidade inteligente e bem-humorada e por sua alta destreza.',
+      'Summer é conhecida por sua personalidade inteligente e bem-humorada e por sua alta destreza',
     foto: require('../assets/img-ram/summer400x400.jpg'),
   },
   {
     nome: 'Jerry Smith',
     descricao:
-      'Jerry sempre tenta pensar no melhor interesse da família, mas possui uma natureza egocêntrica.',
-    foto: require('../assets/img-ram/jerrysmith400x400.png')
+      'Jerry sempre tenta pensar no melhor interesse da família, mas possui uma natureza egocêntrica',
+    foto: require('../assets/img-ram/jerrysmith400x400.png'),
+  },
+  {
+    nome: 'Beth Smith',
+    descricao:
+      'Beth Smith é caracterizada por um complexo de superioridade, impulsionada ainda mais pelo pai, e questões de abandono',
+    foto: require('../assets/img-ram/bethSmith400x400.jpeg'),
+  },
+  {
+    nome: 'Sr. Poopy Butthole',
+    descricao:
+      'Sr. Poopy Butthole é um amigo da Família Smith , que se refugiou com eles para que possa viver uma vida normal',
+    foto: require('../assets/img-ram/srPoopybutthole400x400.jpg'),
   },
 ];
